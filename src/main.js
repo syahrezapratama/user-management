dd14f52dfd94ed5356f10588c5f666fae08aaa21
Syahreza Pratama <syahrezapratama@usv-w26.fh-brandenburg.de>
2022-05-17 17:47:45 +0200

    Add store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import store from './store'
+import store from './store'
 
 import HomePage from './components/HomePage.vue';
 import TheNavigation from './components/TheNavigation.vue';
@@ -13,6 +13,7 @@ import UserData from './components/UserData.vue';
 
 const app = createApp(App);
 
+app.use(store);
 app.use(router);
 app.component('home-page', HomePage);
 app.component('the-navigation', TheNavigation);
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,126 @@
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
+const store = createStore({
+  state() {
+    return {
+      users: [
+        {
+          id: "u1",
+          email: "jake@gmail.com",
+          name: "Jake",
+          postalCode: "12345",
+          city: "Berlin",
+          phone: "017265343",
+          password: "test",
+        },
+        {
+          id: "u2",
+          email: "lisa@gmail.com",
+          name: "Lisa",
+          postalCode: "12346",
+          city: "Berlin",
+          phone: "01783427",
+          password: "test",
+        },
+        {
+          id: "u3",
+          email: "mike@gmail.com",
+          name: "Mike",
+          postalCode: "12335",
+          city: "Potsdam",
+          phone: "017268843",
+          password: "test",
+        },
+        {
+          id: "u4",
+          email: "kevin@gmail.com",
+          name: "Kevin",
+          postalCode: "12375",
+          city: "Hamburg",
+          phone: "0172642343",
+          password: "test",
+        },
+        {
+          id: "u5",
+          email: "vanessa@gmail.com",
+          name: "Vanessa",
+          postalCode: "12875",
+          city: "München",
+          phone: "0172122329",
+          password: "test",
+        },
+        {
+          id: "u6",
+          email: "ben@gmail.com",
+          name: "Ben",
+          postalCode: "12875",
+          city: "München",
+          phone: "0172122329",
+          password: "test",
+        },
+        {
+          id: "u7",
+          email: "adam@gmail.com",
+          name: "Adam",
+          postalCode: "12875",
+          city: "Hannnover",
+          phone: "0172122329",
+          password: "test",
+        },
+        {
+          id: "u8",
+          email: "steve@gmail.com",
+          name: "Steve",
+          postalCode: "12875",
+          city: "München",
+          phone: "0172122329",
+          password: "test",
+        },
+        {
+          id: "u9",
+          email: "cassie@gmail.com",
+          name: "Cassie",
+          postalCode: "12875",
+          city: "Hamburg",
+          phone: "0172122329",
+          password: "test",
+        },
+        {
+          id: "u10",
+          email: "elle@gmail.com",
+          name: "Elle",
+          postalCode: "12875",
+          city: "München",
+          phone: "0172122329",
+          password: "test",
+        },
+        {
+          email: "syahrezarps@gmail.com",
+          name: "Syahreza R. Pratama",
+          postalCode: "14770",
+          city: "Brandenburg an der Havel",
+          phone: "017677900953",
+          password: "12345",
+        },
+      ],
+    };
   },
   getters: {
+    users(state) {
+      return state.users;
+    }
   },
   mutations: {
+    registerUser(state, payload) {
+      state.users.push(payload);
+    },
+    updateUser(state, payload) {
+      state.users = payload;
+    }
   },
   actions: {
   },
   modules: {
   }
-})
+});
+export default store;
